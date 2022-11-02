a647343ef8fa6225d4828078903ed2da5ec34377
yuvraj1902 <yuvraj@gkmit.co>
2022-11-02 12:19:17 +0530

    intial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const express=require('express')
+const db = require('./connection')
+const app=express()
+const dotenv=require('dotenv')
+dotenv.config()
+//const userRouter=require("./routes/user.router")
+db.connect((err)=>{
+    if(err) return err
+    else{
+        console.log("Db connected");
+    }
+})
+app.use(express.json())
+//app.use("/api",userRouter)
+const PORT=process.env.port
+app.listen(PORT,()=>{
+    console.log(`Port is listening on ${PORT}`);
+})
